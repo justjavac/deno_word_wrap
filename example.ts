c3e5a3df56e3505a1f926f8f4aa7197acd5d1058
justjavac <justjavac@gmail.com>
2020-08-21 19:40:39 +0800

    add example

---
+++ example.ts
@@ -0,0 +1,7 @@
+import wrap from "./mod.ts";
+
+const str = wrap(
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+);
+
+console.log(str);
